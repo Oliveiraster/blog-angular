d3c7a1d5355d43a0393f5ed59389b58ad40b2126
Raphael Oliveira <116489080+Oliveiraster@users.noreply.github.com>
2024-01-04 14:01:48 -0300

    alteração de conteudo

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,35 @@
+export const dataFake = [
+    {
+      "id":"1",
+      "photoCover":"https://www.boardinfinity.com/blog/content/images/2023/01/Frontend-vs-Backend.png",
+
+      "title": "FrontEnd vs BackEnd",
+
+      "description":"Qual melhor carreira pra você?",
+
+      "text":"FrontEnd <br>O termo front-end se refere à interface gráfica do usuário (GUI) com a qual as pessoas interagem, como menus de navegação, elementos de design, botões, imagens e gráficos. Em termos técnicos, uma página ou tela que o usuário vê com vários componentes da interface do usuário é chamada de modelo de objeto de documento (DOM).<hr><br> BackEnd <br> Às vezes chamado de lado do servidor, o back-end do aplicativo gerencia a funcionalidade geral do seu aplicativo web. Quando o usuário interage com o front-end, a interação envia uma solicitação para o back-end no formato HTTP. O back-end processa a solicitação e retorna uma resposta."
+    },
+    {
+      "id":"2",
+      "photoCover":"https://attri.com.br/wp-content/uploads/2022/08/qa-quality-assurance-o-que-e-1024x609.webp",
+      "title": "O que é Quality Assurance(QA)?",
+      "description": "Quality Assurance (QA)",
+      "text": "O conceito de Quality Assurance (QA), que pode ser traduzido como “Garantia de Qualidade”, faz referência a um profissional ou uma equipe cuja função é garantir a qualidade no desenvolvimento de um produto ou serviço. Grosso modo, sua atuação envolve a checagem do cumprimento de certos critérios e métodos ao longo dos processos operacionais. <br> O objetivo da sua implantação é identificar desvios que possam comprometer a qualidade final do serviço. Ao mesmo tempo, o QA tem a capacidade de levantar oportunidades, seja uma melhoria direta no serviço, seja um aperfeiçoamento dos processos. <br> Esse monitoramento da qualidade de um produto ou serviço é essencial para fidelizar o cliente e colocar sua empresa em uma posição de destaque no mercado. O QA é uma maneira de alinhar a posição do seu negócio com as expectativas do cliente, garantindo que tudo esteja dentro do padrão de qualidade. <br> Com isso, o Quality Assurance assume um importante papel de acompanhar e assegurar o cumprimento de etapas, assim como sua execução."
+    },
+    {
+      "id":"3",
+      "photoCover":"https://www.gooddata.com/img/blog/_1200x630/project-manager-bg.jpg",
+      "title": "Product Manager ",
+      "description": "O que é um Product Manager? ",
+      "text": "Product Manager precisa manter todas essas equipes e pessoas atualizadas sobre o andamento do projeto, o que também inclui reportar aos superiores sobre o status do produto.<br> <br> Fazer o que chamamos de “stakeholder management” é uma das principais habilidades de qualquer profissional de tecnologia. Ter a capacidade de  passar clareza  dos próximos passos e se certificar de que todos estão com as expectativas alinhadas é chave para ter todos na mesma página. <br> <br> No leque de stakeholders do Product Manager, você vai encontrar lideranças nos seguintes setores dentro das empresas (além do próprio CEO):<br>Vendas; <br> Marketing; <br> Design; <br> Engenharia; <br> Customer Success. <br> <br>Todos estes profissionais têm expectativas sobre como os times de Tecnologia po <br> potencializar as suas áreas. Por isso, elas estarão em constante contato com  Prod <br> Managers. <br> <br> Realizar testes <br> Embora esta seja uma responsabilidade da pessoa de QA (Quality Assurance), Product Managers podem participar da realização de testes no produto (dependendo do tamanho da estrutura que esteja inserida). Isso significa que é muito comum no dia a dia validar se uma solução está sendo entregue sem erro, testar links e botões para se certificar de que os usuários estão tendo sucesso em suas jornadas etc. <br> <br> Analisar dados <br> Os Product Managers são orientados por objetivos e metas, muitas vezes utilizando a estratégia de OKRs. Como veremos mais adiante ainda neste conteúdo, nem sempre o PM terá foco nesta área (dependendo do porte da empresa). Mas é importante que ele saiba, ainda que minimamente, interpretar dados que o ajudem a entender se a experiência do usuário está sendo positiva ou negativa, além de entender se as soluções estão fazendo sentido financeiramente para a empresa (pelo prisma de negócio)."
+      
+    },
+    {
+      "id":"4",
+      "photoCover":"https://i0.wp.com/blog.dsacademy.com.br/wp-content/uploads/2022/04/Analista-de-Dados-%E2%80%93-Por-Onde-Comecar-em-5-Passos.png?fit=1456%2C816&ssl=1",
+      "title": "Analista de Dados",
+      "description": "O que é um Analista de Dados?",
+      "text": "Um Analista de Dados é um profissional responsável por analisar e interpretar grandes volumes de dados, com o objetivo de extrair insights valiosos para a tomada de decisões estratégicas nas empresas. Esse profissional utiliza técnicas avançadas de análise de dados, estatística e programação para identificar padrões, tendências e correlações que possam ser utilizados para impulsionar o crescimento e o sucesso das organizações."
+      
+    }
+  ]
